dab44b84cc8a21508c0bf93312b828951f570cd2
alisa <alisa.dammer@gmail.com>
2020-12-21 22:26:50 +0100

    added medication struct and some basic functions, test. Cli will be implenmenting some pasrings as own implementation decision

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,5 @@
 mod meds;
-mod activity;
-mod cli;
-use cli::{Tracker, EntryType, save_to_file};
-use strum::IntoEnumIterator;
-use structopt::StructOpt;
-use std::process;
-use std::error::Error;
 
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Tracker::from_args();     
-    match args {
-        Tracker::List => {
-            let mut available_forms = String::from("Available tracking forms are: ");
-            for t in EntryType::iter() {
-                available_forms.push_str(&t.to_string());
-            }
-            println!("{}", available_forms);
-        },
-        Tracker::Info => {
-            println!("Here the general info for the project and each tracking will be shown");
-        },
-        Tracker::Save{file, entrytype, data} => {
-            if let Err(err) = save_to_file(entrytype, &data, file) {
-                println!("Could not save entry due to {}", err);
-                process::exit(1);
-            }
-        },
-        Tracker::Show{entrytype, usage} => {
-            match entrytype {
-                EntryType::Medication => {
-                    println!("{}", meds::show_info(&usage));
-                },
-                EntryType::Activity => {
-                    println!("{}", activity::show_info(&usage));
-                },
-            }
-        }
-    }
-    Ok(())
+fn main() {
+    println!("{}", "Hello!");
 }
--- src/meds.rs
+++ src/meds.rs
@@ -1,90 +1,63 @@
 use chrono::{DateTime, Utc};
 use strum_macros;
-use std::str::FromStr;
-use strum::EnumString;
-use serde::Serialize;
-use crate::cli::InformationMode;
 
+/// # Medication Tracking
+///
+/// simple way to collect basic information about the 
+///
+/// TODO: comments can belong to tags like "sideeffect", "reason", "etc" 
+///
+///
 
-#[derive(Debug, strum_macros::ToString, EnumString, Clone, Serialize)]
-#[serde(rename_all="lowercase")]
-pub enum MedDosageUnit {
-    #[strum(serialize="mg")]
-    MG,
-    #[strum(serialize="g")]
-    G
+#[derive(strum_macros::ToString, Debug)]
+pub enum Unit {
+    #[strum(serialize = "mg")]
+    Mg, 
+    #[strum(serialize = "g")]
+    G, 
+    #[strum(serialize = "ml")]
+    Ml, 
+    #[strum(serialize = "l")]
+    L
 }
 
 
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub struct MedikamentationForm {
+pub struct Medication {
     pub name: String,
-    pub dosage: i32,
-    pub unit: MedDosageUnit,
-    pub day_part: Option<String>,
-    pub reason: Option<String>,
-    pub side_effects: Option<String>,
-    timestamp: String,  // TODO: write proprt setter to operate on private field in the code
+    pub dosage: i16,
+    pub unit: Unit,
+    pub timestamp: DateTime<Utc>,
+    pub comment: Option<String>  // TODO: set tagging for comments: Tag::SideEffect, "headache" or Tag::Reason, "prescription"  
 }
 
-
-impl FromStr for MedikamentationForm {
-    type Err = std::string::ParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut args: Vec<String> = s.to_lowercase().split(',').map(|w| w.trim().to_string()).collect();
-        if args.len() < 3 {
-            panic!("Medical record should contain at least 3 non-empty strings devided by ','")
-        };
-        let mut side_effects = None;
-        let mut reason = None;
-        let mut day_part = None;
-        let mut unit = MedDosageUnit::MG;
-        let mut dosage = 0;
-        let mut name = String::from("");
-        match args.len() {
-            // Is it better/faster to use several ifs and use pop -> go over all conditions?
-            6 => {
-                side_effects = args.pop();
-                reason = args.pop();        
-                day_part = args.pop();
-            },
-            5 => {
-                reason = args.pop();        
-                day_part = args.pop();
-            },
-            4 => {
-                day_part = args.pop();
-                unit = MedDosageUnit::from_str(&args.pop().unwrap()).unwrap();
-                dosage = (args.pop().unwrap()).parse::<i32>().unwrap();
-                name = args.pop().unwrap();
-            },
-            3 => {
-                unit = MedDosageUnit::from_str(&args.pop().unwrap()).unwrap();
-                dosage = (args.pop().unwrap()).parse::<i32>().unwrap();
-                name = args.pop().unwrap();
-            },
-            _ => {panic!("The medical record is too short")}
-        };
-    
-
-        Ok(MedikamentationForm{name, dosage, unit, day_part, reason, side_effects, timestamp: Utc::now().to_string()})
+impl Medication {
+    /// Constructor method 
+    // TODO: do i need to create 2 constructors to ignore comment completely?
+    pub fn new(name: String, dosage: i16, unit: Unit, timestamp: DateTime<Utc>, comment: Option<String>) -> Medication {
+        let res = Medication{name, dosage, unit, timestamp, comment};
+        res
     }
-
+        
 }
 
 
-pub fn show_info(s: &InformationMode) -> String {
-    let mut res = String::new();
-    match s {
-        &InformationMode::Info => {
-            res = String::from("Available fields: name: str, dosage: number, unit: str (mg/g), (optional)time: str, (optinonal)reason: str, (optional)side_effects: str");
-        },
-        &InformationMode::Example => {
-            res = String::from("Ibuprofen, 400, mg, evening, headache, drowsy");
-        }
-    };
-    res
+impl ToString for Medication {
+    fn to_string(&self) -> String {
+        let res: String = format!("name: {}, dosage: {}, unit: {}, timestamp: {}", 
+                                  &self.name.clone(), &self.dosage, 
+                                  &self.unit.to_string(), &self.timestamp.to_string());
+        res
+    }
+}
+
+
+
+#[test]
+fn positive_to_string() {
+    let today = Utc::now();
+    let medication = Medication::new(String::from("Aspirin"), 200, Unit::Mg, today, None);
+    let expcted: String = String::from(format!("name: Aspirin, dosage: 200, unit: mg, timestamp: {}", today.to_string()));
+    assert_eq!(medication.to_string(), expcted); 
+
 }
 
